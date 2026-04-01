8bffd088a11c8fda8916cbddef5e34318b87ebea
Mashrafi Bin Nur <mbngms@gmail.com>
2026-04-02 01:01:13 +0600

    cookie updates

--- app.js
+++ app.js
@@ -442,10 +442,22 @@ async function run() {
             })
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                secure: true,
+                sameSite: 'none',
             }).send({ success: true })
         })
+
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '365d'
+        //     })
+        //     res.cookie('token', token, {
+        //         httpOnly: true,
+        //         secure: process.env.NODE_ENV === 'production',
+        //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //     }).send({ success: true })
+        // })
 
         // app.get('/logout', async (req, res) => {
         //     try {
