d1c213d14c30f304feb172cc6263b98613c06a8c
Mashrafi Bin Nur <mbngms@gmail.com>
2026-04-02 01:33:28 +0600

    new route added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://mcpitc.web.app'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://mcpitc.web.app', 'https://mcpscictclub.com'],
     credentials: true,
     optionsSuccessStatus: 200
 }
